0119424f5109633529392cab4e1f511c3e44416f
abhi62 <abhi.leon.93@gmail.com>
2022-03-02 11:29:49 +0530

    bootcamp routs

--- server.js
+++ server.js
@@ -4,8 +4,34 @@ const dotenv = require('dotenv');
 //loading env
 
 dotenv.config({ path: './config/config.env' });
-
 const app = express();
+// creating rout
+
+app.get('/api/v1/bootcamps', (req, res) => {
+  // res.send({ name: 'brad' });
+  res.status(200).send({ success: true, msg: 'show all bootcamps' });
+});
+app.get('/api/v1/bootcamps/:id', (req, res) => {
+  res
+    .status(200)
+    .send({ success: true, msg: `showing  bootcamp ${req.params.id}` });
+});
+app.post('/api/v1/bootcamps', (req, res) => {
+  // res.send({ name: 'brad' });
+  res.status(200).send({ success: true, msg: 'create new bootcamps' });
+});
+app.put('/api/v1/bootcamps/:id', (req, res) => {
+  res
+    .status(200)
+    .send({ success: true, msg: `update bootcamp ${req.params.id}` });
+});
+app.delete('/api/v1/bootcamps/:id', (req, res) => {
+  // res.send({ name: 'brad' });
+  res
+    .status(200)
+    .send({ success: true, msg: `delete bootcamp ${req.params.id}` });
+});
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
