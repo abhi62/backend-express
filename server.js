810c169022becfd5161d02ac4b70e73ced68df2e
abhi62 <abhi.leon.93@gmail.com>
2022-03-02 11:04:09 +0530

    express initial commit

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const dotenv = require('dotenv');
+
+//loading env
+
+dotenv.config({ path: './config/config.env' });
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(
+    `server started at port ${PORT} in ${process.env.NODE_ENV} environment`
+  );
+});
